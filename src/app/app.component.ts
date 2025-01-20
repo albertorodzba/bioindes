72aed883c6075cbf28c662e1091ed240c259520d
albertorodzba <alberto_rdzbautista@hotmail.com>
2025-01-20 02:59:31 -0600

    [feat]: link home added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {NavbarComponent} from "./core/components/navbar/navbar.component";
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./core/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/core/components/navbar-links/navbar-links.component.ts
+++ src/app/core/components/navbar-links/navbar-links.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {NavbarLink} from "../../models/navbarLink.interface";
+import { NavbarLink } from "../../models/navbarLink.interface";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'navbar-links',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive,],
   templateUrl: './navbar-links.component.html',
   styleUrl: './navbar-links.component.scss'
 })
