95e11cf6ca04f587c0225c34a3ea396758c9be88
albertorodzba <alberto_rdzbautista@hotmail.com>
2025-02-03 04:34:14 -0600

    [Feat]: services section added

--- src/app/core/components/navbar-links/navbar-links.component.ts
+++ src/app/core/components/navbar-links/navbar-links.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { NavbarLink } from "../../models/navbarLink.interface";
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import {JsonPipe} from "@angular/common";
@@ -15,24 +15,30 @@ export class NavbarLinksComponent {
   sections: NavbarLink[] = Sections;
   isMouseEnter: boolean = false;
   currentLinkSelected: any = [];
+  @ViewChild("navbar_li") navbarLi!: ElementRef;
+  @ViewChild("submenu") submenu!: ElementRef ;
 
   onMouseEnter(linkName: string ) {
-    console.log(linkName, "pulsed");
+    console.log(linkName, "entered");
     this.isMouseEnter = true;
     this.currentLinkSelected = this.sections.filter((element) => element.name === linkName);
-    console.log("current selected", this.currentLinkSelected);
+    if(this.currentLinkSelected.length > 0){
+      this.navbarLi?.nativeElement.appendChild(this.submenu);
+    }
   }
 
   onMouseLeave(){
     this.isMouseEnter = false;
     this.currentLinkSelected = [];
+    console.log("leaveee");
   }
+
   @HostListener("document:click", ["$event"])
   onClickListener(event: any) {
-    console.log("target", event.target.className);
+    // console.log("target", event.target.className);
     let targetClassNameClicked: string = event.target.className;
     if (targetClassNameClicked.includes("navbar")) {
-      console.log("targetActive", event);
+      // console.log("targetActive", event);
     } else {
       this.isMouseEnter = false;
     }
