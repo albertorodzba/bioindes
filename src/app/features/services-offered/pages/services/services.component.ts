1185cce6515709d45a15a4ab0506c1d3dd35eef7
albertorodzba <alberto_rdzbautista@hotmail.com>
2025-02-06 00:43:22 -0600

    [Style]: Responsive services section

--- src/app/features/services-offered/pages/services/services.component.ts
+++ src/app/features/services-offered/pages/services/services.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {servicios} from "../../../../core/data/servicios-info";
 import {DualPanelCardComponent} from "../../../../shared/components/dual-panel-card/dual-panel-card.component";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-services',
   standalone: true,
   imports: [
-    DualPanelCardComponent
+    DualPanelCardComponent,
+    NgClass
   ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
